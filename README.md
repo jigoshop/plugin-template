bb7d3d3391882358ca1ee60a9a3236ba726c68db
kkasowski <krzysztof@kasowski.net>
2016-01-21 14:24:02 +0100

    init repository

---
+++ README.md
@@ -0,0 +1,9 @@
+## Jigoshop Plugin Init file template
+
+### Usage
+
+* Replace all `PLUGIN_MAME` in constants.
+* Replace all `PluginName` in file paths.
+* Change plugin textdomain.
+* Change product id in license validaros, or remove them completely.
+* Change documentation url.
---
+++ bootstrap.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Plugin Name:
+ * Plugin URI:
+ * Description:
+ * Version: 1.0
+ * Author: Jigoshop
+ * Author URI: https://www.jigoshop.com/
+ * Init File Version: 1.0
+ * Init File Date: 2015-10-07
+ */
+
+// Define plugin directory for inclusions
+if (!defined('JIGOSHOP_PLUGIN_NAME_DIR')) {
+    define('JIGOSHOP_PLUGIN_NAME_DIR', dirname(__FILE__));
+}
+// Define plugin URL for assets
+if (!defined('JIGOSHOP_PLUGIN_NAME_URL')) {
+    define('JIGOSHOP_PLUGIN_NAME_URL', plugins_url('', __FILE__));
+}
+// Define plugin name
+if (!defined('JIGOSHOP_PLUGIN_NAME_NAME')) {
+    define('JIGOSHOP_PLUGIN_NAME_NAME', 'Plugin Name');
+}
+
+add_action('plugins_loaded', function () {
+    load_plugin_textdomain('plugin_textdomain', false, dirname(plugin_basename(__FILE__)) . '/languages/');
+
+    if (class_exists('\Jigoshop\Core')) {
+        //Check version.
+        if (\Jigoshop\addRequiredVersionNotice(JIGOSHOP_PLUGIN_NAME_NAME, '2.0')) {
+            return;
+        }
+
+        //Check license.
+        $licence = new \Jigoshop\Licence(__FILE__, '13190', 'http://www.jigoshop.com');
+        if (!$licence->isActive()) {
+            return;
+        }
+
+        //Init components.
+        require_once(JIGOSHOP_PLUGIN_NAME_DIR . '/src/PluginName.common.php');
+        if (is_admin()) {
+            require_once(JIGOSHOP_PLUGIN_NAME_DIR . '/src/PluginName.backend.php');
+        } else {
+            require_once(JIGOSHOP_PLUGIN_NAME_DIR . '/src/PluginName.frontend.php');
+        }
+
+    } elseif (class_exists('jigoshop')) {
+        //Check version.
+        if (jigoshop_add_required_version_notice(JIGOSHOP_PLUGIN_NAME_NAME, '1.17')) {
+            return;
+        }
+
+        //Check license.
+        $licence = new jigoshop_licence_validator(__FILE__, '13190', 'http://www.jigoshop.com');
+        if (!$licence->is_licence_active()) {
+            return;
+        }
+
+        //Init components.
+        require_once(JIGOSHOP_PLUGIN_NAME_DIR . '/src/PluginName-1x.common.php');
+        if (is_admin()) {
+            require_once(JIGOSHOP_PLUGIN_NAME_DIR . '/src/PluginName-1x.backend.php');
+        } else {
+            require_once(JIGOSHOP_PLUGIN_NAME_DIR . '/src/PluginName-1x.frontend.php');
+        }
+
+    } else {
+        add_action('admin_notices', function () {
+            echo '<div class="error"><p>';
+            printf(__('%s requires Jigoshop plugin to be active. Code for plugin %s was not loaded.',
+                'plugin_textdomain'), JIGOSHOP_PLUGIN_NAME_NAME);
+            echo '</p></div>';
+        });
+    }
+});
+
+if (is_admin()) {
+    add_filter('plugin_action_links_' . plugin_basename(__FILE__), function ($links) {
+        $links[] = '<a href="https://www.jigoshop.com/documentation/plugin_name" target="_blank">Documentation</a>';
+        $links[] = '<a href="https://www.jigoshop.com/support/" target="_blank">Support</a>';
+        $links[] = '<a href="https://wordpress.org/support/view/plugin-reviews/jigoshop#postform" target="_blank">Rate Us</a>';
+        $links[] = '<a href="https://www.jigoshop.com/product-category/extensions/" target="_blank">More plugins for Jigoshop</a>';
+
+        return $links;
+    });
+}
