7589627fd7b356dee680097bca4adb5e91f5e473
Tomasz Dyda <nofacexx@gmail.com>
2016-04-01 12:11:48 +0200

    ~~ change initial file name ~~ shorten the path

--- bootstrap.php
+++ bootstrap.php
@@ -35,11 +35,11 @@ add_action('plugins_loaded', function () {
         define('JIGOSHOP_PLUGIN_NAME_URL', plugins_url('', __FILE__));
 
         //Init components.
-        require_once(JIGOSHOP_PLUGIN_NAME_DIR . '/src/Jigoshop/Extension/PluginName/common.php');
+        require_once(JIGOSHOP_PLUGIN_NAME_DIR . '/src/Jigoshop/Extension/PluginName/Common.php');
         if (is_admin()) {
-            require_once(JIGOSHOP_PLUGIN_NAME_DIR . '/src/Jigoshop/Extension/PluginName/backend.php');
+            require_once(JIGOSHOP_PLUGIN_NAME_DIR . '/src/Jigoshop/Extension/PluginName/Admin.php');
         } else {
-            require_once(JIGOSHOP_PLUGIN_NAME_DIR . '/src/Jigoshop/Extension/PluginName/frontend.php');
+            require_once(JIGOSHOP_PLUGIN_NAME_DIR . '/src/Jigoshop/Extension/PluginName/Frontend.php');
         }
 
     } elseif (class_exists('jigoshop')) {
@@ -61,11 +61,11 @@ add_action('plugins_loaded', function () {
         define('JIGOSHOP_PLUGIN_NAME_URL', plugins_url('', __FILE__) . '/Jigoshop1x');
 
         //Init components.
-        require_once(JIGOSHOP_PLUGIN_NAME_DIR . '/Jigoshop1x/src/to/plugin/common/code.php');
+        require_once(JIGOSHOP_PLUGIN_NAME_DIR . '/src/to/plugin/common/code.php');
         if (is_admin()) {
-            require_once(JIGOSHOP_PLUGIN_NAME_DIR . '/Jigoshop1x/src/to/plugin/backend/code.php');
+            require_once(JIGOSHOP_PLUGIN_NAME_DIR . '/src/to/plugin/backend/code.php');
         } else {
-            require_once(JIGOSHOP_PLUGIN_NAME_DIR . '/Jigoshop1x/src/to/plugin/front/code.php');
+            require_once(JIGOSHOP_PLUGIN_NAME_DIR . '/src/to/plugin/front/code.php');
         }
 
     } else {
